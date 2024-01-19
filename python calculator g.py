af4ec8b8620f18ddd6a1c654c3740166fdf8193a
tanianaeem123 <157149877+tanianaeem123@users.noreply.github.com>
2024-01-19 22:32:44 +0500

    Add files via upload

---
+++ python calculator g.py
@@ -0,0 +1,23 @@
+a=int(input("Enter a first number:  "))
+
+b=int(input("Enter a second number:  "))
+
+oper=input("Enter a operator:  ")
+
+if oper=="+":
+    add=a+b
+    print(add)
+
+if oper=="-":
+    sub=a-b
+    print(sub)
+
+if oper=="*":
+    multi=a*b
+    print(multi)
+
+if oper=="/":
+    divI=a/b
+    print(divI)
+    
+    
